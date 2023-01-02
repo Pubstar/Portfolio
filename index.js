6f580c2c5c09238c5024c768660f1df5eae18515
Pubstar <pieterhoek@msn.com>
2023-01-02 20:37:32 +0100

    Fixed "contact" nav link on mobile
    Button did not work, now it closes the nav overlay and jumps to the contact section

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const burgerIcon = document.querySelector('.hamburger-icon');
 const overlay = document.querySelector('.nav-overlay');
+const mobileContact = document.querySelector('.mobile-contact');
 
 let overlayOn = false;
 
 burgerIcon.addEventListener('click', () => {
+    toggleOverlay();
+})
+
+mobileContact.addEventListener('click', () => {
+    toggleOverlay();
+})
+
+function toggleOverlay() {
     if (overlayOn) {
         overlay.style.visibility = 'hidden';
         overlay.style.opacity = 0;
@@ -14,18 +23,18 @@ burgerIcon.addEventListener('click', () => {
         disableScroll();
     }
     overlayOn = !overlayOn;
-})
+}
 
 function disableScroll() {
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  
-        window.onscroll = function() {
+
+        window.onscroll = function () {
             window.scrollTo(scrollLeft, scrollTop);
         };
 }
-  
+
 function enableScroll() {
-    window.onscroll = function() {};
+    window.onscroll = function () { };
 }
 
